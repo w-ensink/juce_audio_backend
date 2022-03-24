f6b769806d0cb9b90fa24387df1129879fbb32a5
Wouter Ensink <wouter@ensink.io>
2022-03-24 16:32:18 +0100

    initial commit

---
+++ juce_audio.cpp
@@ -0,0 +1,78 @@
+
+#include "juce_audio.h"
+#include <juce_audio_devices/juce_audio_devices.h>
+
+struct AudioBackend::Pimpl : juce::AudioIODeviceCallback
+{
+    void openDefaultIODevice (int numInputChannels, int numOutputChannels)
+    {
+        audioDeviceManager.addAudioCallback (this);
+        audioDeviceManager.initialiseWithDefaultDevices (numInputChannels, numOutputChannels);
+    }
+
+    void setCallback (AudioCallback* audioCallback)
+    {
+        callback = audioCallback;
+
+        if (callback != nullptr)
+            callback->prepareToPlay (sampleRate, blockSize);
+    }
+
+    void closeDevice()
+    {
+        audioDeviceManager.closeAudioDevice();
+    }
+
+private:
+    AudioCallback* callback = nullptr;
+    juce::AudioDeviceManager audioDeviceManager;
+    double sampleRate = 0.0;
+    int blockSize = 0;
+
+    void audioDeviceIOCallback (const float** inputChannelData, int numInputChannels, float** outputChannelData, int numOutputChannels, int numSamples) override
+    {
+        if (callback != nullptr)
+            callback->process (inputChannelData, outputChannelData, numInputChannels, numOutputChannels, numSamples);
+    }
+
+    void audioDeviceAboutToStart (juce::AudioIODevice* device) override
+    {
+        sampleRate = device->getCurrentSampleRate();
+        blockSize = device->getCurrentBufferSizeSamples();
+    }
+
+    void audioDeviceStopped() override
+    {
+        if (callback != nullptr)
+            callback->releaseResources();
+    }
+
+    void audioDeviceError (const juce::String& errorMessage) override
+    {
+        std::cerr << "audio device error: " << errorMessage << "\n";
+    }
+};
+
+
+void AudioBackend::PimplDeleter::operator() (Pimpl* toDelete)
+{
+    delete toDelete;
+}
+
+
+AudioBackend::AudioBackend() : pimpl (std::unique_ptr<Pimpl, PimplDeleter> { new Pimpl() }) {}
+
+void AudioBackend::openDefaultIODevice (int numInputChannels, int numOutputChannels)
+{
+    pimpl->openDefaultIODevice (numInputChannels, numOutputChannels);
+}
+
+void AudioBackend::registerCallback (AudioCallback* callback)
+{
+    pimpl->setCallback (callback);
+}
+
+void AudioBackend::closeDevice()
+{
+    pimpl->closeDevice();
+}
---
+++ test_main.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <juce_audio.h>
+#include <random>
+#include <thread>
+
+struct TestCallback : AudioCallback
+{
+    void process (const float** input, float** output, int numInputChannels, int numOutputChannels, int numSamples) override
+    {
+        for (int channel = 0; channel < numOutputChannels; ++channel)
+        {
+            for (int sample = 0; sample < numSamples; ++sample)
+            {
+                output[channel][sample] = static_cast<float> (distribution (generator)) * 0.2f;
+            }
+        }
+    }
+
+private:
+    std::random_device randomDevice;
+    std::mt19937 generator { randomDevice() };
+    std::uniform_real_distribution<> distribution { 0.0, 1.0 };
+};
+
+
+int main()
+{
+    TestCallback callback;
+    AudioBackend audioBackend;
+
+    audioBackend.registerCallback (&callback);
+    audioBackend.openDefaultIODevice (0, 2);
+
+    std::this_thread::sleep_for (std::chrono::seconds (2));
+
+    audioBackend.closeDevice();
+
+    return 0;
+}
